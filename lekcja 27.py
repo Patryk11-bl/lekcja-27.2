2e285910953d918c70a6e481b5e179557b587069
Patryk11-bl <bloch.patryk.pb1@gmail.com>
2025-05-10 15:56:42 +0200

    minimalne zmiany

--- lekcja 27.py
+++ lekcja 27.py
@@ -6,12 +6,14 @@ try:
 
 
 
-    plik.write('Y')
+    plik.read('Y')
     plik.close()
 
 except FileNotFoundError as e:
     print('Plik nie istnieje')
     print(e)
+
+
 except Exception as e:
     print('Nieznany błąd')
     print(e)
